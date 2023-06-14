d0d305afe755881d485ec78d45eea20b2196bb3e
Abhinav <11015433@stud.hochschule-heidelberg.de>
2023-06-14 11:47:56 +0200

    Updated Commit

---
+++ DoctorsData.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        name:"Dr. Sara Tancredi",
+        imgUrl:require('./assets/DrSara.jpg'),
+        type:'speech therapist',
+        rating:4,
+        id:1
+    },
+    {
+        name:"Dr. Stephen",
+        imgUrl:require('./assets/DrStephen.jpg'),
+        type:'pediatric neurologists',
+        rating:4,
+        id:2
+    },
+    {
+        name:"Dr. Sara Tancredi",
+        imgUrl:require('./assets/DrSara.jpg'),
+        type:'speech therapist',
+        rating:4,
+        id:3
+    },
+    {
+        name:"Dr. Stephen",
+        imgUrl:require('./assets/DrStephen.jpg'),
+        type:'pediatric neurologists',
+        rating:4,
+        id:4
+    }
+]
+
+
+export default data
